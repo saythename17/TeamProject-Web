45fb2981fb5f72f9c21668310e671df7341e51eb
FANXY_ON <zion.h719@gmail.com>
2020-08-27 08:34:05 +0900

    TODO : Response & dataadd

--- js/artist.js
+++ js/artist.js
@@ -42,6 +42,10 @@ var artistInfo=a=>{
             document.getElementById('birth').innerText = ""+artists[a].birthday
             document.getElementById('fancafe').href = ""+artists[a].fancafe
             document.getElementById('snspage').href = ""+artists[a].sns
+
+
+            // video
+            document.getElementById('video-frame').src=""+artists[a].mainVideo
         }
     }
     req.open('POST','./artist.json',true)
@@ -85,6 +89,48 @@ var video=()=>{
 
 
 
+
+
+
+
+
+
+
+
+//slide page
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("pageNum");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
 // function tabsHandler(index) {
 //     return function tabClickEvent() {
 //         var title=document.getElementsByClassName('video-title')[index]
@@ -104,67 +150,3 @@ var video=()=>{
 
 
 
-
-// video -page youtube bottom list slide
-const slideList = document.querySelector('.slide_list'); // Slide parent dom
-const slideContents = document.querySelectorAll('.slide_content'); // each slide dom
-const slideBtnNext = document.querySelector('.slide_btn_next'); // next button
-const slideBtnPrev = document.querySelector('.slide_btn_prev'); // prev button
-const slideLen = slideContents.length; // slide length
-const slideWidth = 400; // slide width
-const slideSpeed = 300; // slide speed
-const startNum = 0; // initial slide index (0 ~ 4)
-slideList.style.width = slideWidth * (slideLen + 2) + "px";
-
-
-// Copy first and last slide
-let firstChild = slideList.firstElementChild;
-let lastChild = slideList.lastElementChild;
-let clonedFirst = firstChild.cloneNode(true);
-let clonedLast = lastChild.cloneNode(true);
-
-
-
-// Add copied Slides
-slideList.appendChild(clonedFirst);
-slideList.insertBefore(clonedLast, slideList.firstElementChild);
-slideList.style.transform = "translate3d(-" + (slideWidth * (startNum + 1)) + "px, 0px, 0px)";
-let curIndex = startNum; // current slide index (except copied slide)
-let curSlide = slideContents[curIndex]; // current slide dom
-curSlide.classList.add('slide_active');
-
-
-/** Next Button Event */
-slideBtnNext.addEventListener('click', function() {
-if (curIndex <= slideLen - 1) {
-    slideList.style.transition = slideSpeed + "ms";
-    slideList.style.transform = "translate3d(-" + (slideWidth * (curIndex + 2)) + "px, 0px, 0px)";
-}
-if (curIndex === slideLen - 1) {
-    setTimeout(function() {
-    slideList.style.transition = "0ms";
-    slideList.style.transform = "translate3d(-" + slideWidth + "px, 0px, 0px)";
-}, slideSpeed);
-curIndex = -1;
-}
-curSlide.classList.remove('slide_active');
-curSlide = slideContents[++curIndex];
-curSlide.classList.add('slide_active');
-});
-/** Prev Button Event */
-slideBtnPrev.addEventListener('click', function() {
-if (curIndex >= 0) {
-    slideList.style.transition = slideSpeed + "ms";
-    slideList.style.transform = "translate3d(-" + (slideWidth * curIndex) + "px, 0px, 0px)";
-}
-if (curIndex === 0) {
-    setTimeout(function() {
-        slideList.style.transition = "0ms";
-        slideList.style.transform = "translate3d(-" + (slideWidth * slideLen) + "px, 0px, 0px)";
-    }, slideSpeed);
-    curIndex = slideLen;
-    }
-    curSlide.classList.remove('slide_active');
-    curSlide = slideContents[--curIndex];
-    curSlide.classList.add('slide_active');
-});
